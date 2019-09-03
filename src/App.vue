a08b3c461f7023aa08a5edf4ed4860e2a8bfeacc
kammradt <vinicius.kammradt1@gmail.com>
2019-09-03 17:31:26 -0300

    :sparkles: Creating dashboard and components
    Now the structure of the dashboard is done. It get all tasks at the monuted() hook, and allow the user to search using api. I also created some components to help spitting the code. I will create some extra functions to allow the user creating and deleting tasks, but I need to create those functions inside our API. Now I will verify how to deal with Trello's API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,100 @@
 <template>
-  
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="12">
+            <v-card class="ma-1 mb-4">
+              <v-card-text>
+                <v-text-field label="Type todo's name . . ." v-model="search.searchField" />
+              </v-card-text>
+              <v-card-actions>
+                <v-btn @click="searchByName" block class="primary">Search</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <template v-if="search.searchResult">
+            <TaskCard
+              :task="search.searchResult"
+              :key="search.searchResult.id"
+              :title="search.searchResult.taskName"
+              :color="'teal lighten-5'"
+            />
+          </template>
+          <template v-else-if="search.notFound">
+            <TaskCard
+              :task="search.possibleNewTask"
+              :key="2314"
+              :title="search.possibleNewTask.taskName"
+              :color="'yellow lighten-5'"
+              :newTask="true"
+            />
+          </template>
+        </v-row>
+
+        <v-row>
+          <template v-for="task in tasks">
+            <TaskCard :task="task" :key="task.id" :title="task.taskName" />
+          </template>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import TaskCard from '@/components/TaskCard'
 
+export default {
+  name: 'App',
+  components: {
+    TaskCard
+  },
+  data() {
+    return {
+      tasks: [],
+      search: {
+        searchField: '',
+        searchResult: null,
+        notFound: false,
+        possibleNewTask: {
+          taskName: '',
+          listName: 'TODO',
+          createdAt: new Date()
+        }
+      }
+    }
+  },
+  created() {
+    this.getAll()
+  },
+  methods: {
+    getAll() {
+      axios.get('http://localhost:8080/task').then(response => {
+        this.tasks = response.data
+      })
+    },
+    searchByName() {
+      axios
+        .get('http://localhost:8080/task/search', {
+          params: {
+            taskName: this.search.searchField
+          }
+        })
+        .then(response => {
+          if (response.data) {
+            this.search.searchResult = response.data
+            this.search.searchField = ''
+          } else {
+            this.search.searchResult = null
+            this.search.notFound = true
+            this.search.possibleNewTask.taskName = this.search.searchField
+            this.search.searchField = 'Wrong name! Want to add?'
+          }
+        })
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col cols="12" md="3" sm="6">
+    <v-card :color="color">
+      <v-card-title>
+        {{ title }}
+        <span class="pl-1 caption grey--text">{{ task.listName }}</span>
+      </v-card-title>
+      <v-card-text>Date created {{ (new Date(task.createdAt)).toLocaleDateString("br-PT") }}</v-card-text>
+      <v-card-actions>
+        <v-btn v-if="!newTask" class="error">
+          Delete
+        </v-btn>
+        <v-btn @click="createNewTask" v-else class="primary">
+          Click to add
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  props: {
+    task: Object,
+    title: String,
+    color: String,
+    newTask: Boolean
+  },
+  methods: {
+    createNewTask() {
+      // axios.post()
+      // Post para parte da api que chama a api do trello
+    }
+  }
+}
+</script>
